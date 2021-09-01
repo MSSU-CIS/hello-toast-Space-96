a8de71b6281747a703b903b88770c8f2b5ab6c2d
Brendan Bernal <brendanbernal@live.com>
2021-09-01 16:07:19 -0500

    1.2 PartB

--- app/src/main/java/edu/mssu/cis385/hellotoast/MainActivity.java
+++ app/src/main/java/edu/mssu/cis385/hellotoast/MainActivity.java
@@ -3,12 +3,56 @@ package edu.mssu.cis385.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    private int mZero = 0;
+    private Button mButtonCount;
+    private View view1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButtonCount = (Button) findViewById(R.id.button_count);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        view1 = (View) findViewById(R.id.zero_button);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        view1.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+        if (mCount % 2 == 0){
+            view.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        }
+        else{
+            view.setBackgroundColor(getResources().getColor(R.color.teal_200));
+        }
+    }
+
+    public void showZero(View view) {
+        mShowCount.setText(Integer.toString(mZero));
+        mCount = 0;
+        view1.setBackgroundColor(getResources().getColor(R.color.black));
+        if (mCount % 2 == 0){
+            mButtonCount.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        }
+        else{
+            mButtonCount.setBackgroundColor(getResources().getColor(R.color.teal_200));
+        }
     }
 }
